e61cd51f89775d74bfb49e23fca831c8b0ddadda
ilya <isdmitriev@mail.ru>
2018-07-21 17:19:12 +0300

    Second

---
+++ Rep/code/MyInterface.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rep.code
+{
+    interface MyInterface
+    {
+        List<int> getAllProducts();
+    }
+}
